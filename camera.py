2c68611f3f3e5da647f7d49d8920b559d60b5427
Issac Kelly <issac.kelly@gmail.com>
2014-07-04 18:23:37 -0700

    Camera Update to new format.

--- camera.py
+++ camera.py
@@ -2,23 +2,10 @@ from SimpleCV import Camera
 from PIL import  Image
 import sys
 import opc, sys, time
-from utils import simplecv_smart_crop
+from utils import
+from utils import simplecv_smart_crop, layout, size
 
-size = 32, 16
 client = opc.Client('localhost:7890')
-
-
-sets = (
-    [[0,0], [7,7]],
-    [[8,0], [15,7]],
-    [[16,0],[23,7]],    #  0 1 2 3
-    [[24,0],[31,7]],    #  4 5 6 7
-
-    [[0,8], [7,15]],
-    [[8,8], [15,15]],
-    [[16,8],[23, 15]],
-    [[24,8],[31,15]],
-)
 
 
 # Initialize the camera
@@ -34,19 +21,19 @@ while True:
     im = im.flipHorizontal()
     im = im.resize(*size)
 
-    lena = []
+    frame = []
 
-    for start, end in sets:
+    for start, end in layout:
         i = start[0]
         j = start[1]
         while j <= end[1]:
             while i <= end[0]:
-                lena.append(im.getPixel(i,j))
+                frame.append(im.getPixel(i,j))
                 i+=1
             j+=1
             i = start[0]
 
     # Display the pixels
-    client.put_pixels(lena)
+    client.put_pixels(frame)
     time.sleep(0.15)
 
